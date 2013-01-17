56ff10669e5d09fb01fbc0f67be8fe86c9da8f45
Jeff Terrace <jterrace@google.com>
2013-01-17 13:56:26 -0800

    Add tests for set_contents_from_stream.

--- tests/integration/gs/test_generation_conditionals.py
+++ tests/integration/gs/test_generation_conditionals.py
@@ -147,3 +147,25 @@ class GSGenerationConditionalsTest(GSTestCase):
 
         b.set_acl("public-read", key_name="foo", if_generation=g3,
                   if_metageneration=mg3)
+
+    def testConditionalSetContentsFromStream(self):
+        b = self._MakeBucket()
+        k = b.new_key("foo")
+        s1 = "test1"
+        fp = StringIO.StringIO(s1)
+        with self.assertRaisesRegexp(GSResponseError, VERSION_MISMATCH):
+            k.set_contents_from_stream(fp, if_generation=999)
+
+        fp = StringIO.StringIO(s1)
+        k.set_contents_from_stream(fp, if_generation=0)
+        g1 = k.generation
+
+        k = b.get_key("foo")
+        s2 = "test2"
+        fp = StringIO.StringIO(s2)
+        with self.assertRaisesRegexp(GSResponseError, VERSION_MISMATCH):
+            k.set_contents_from_stream(fp, if_generation=int(g1)+1)
+
+        fp = StringIO.StringIO(s2)
+        k.set_contents_from_stream(fp, if_generation=g1)
+        self.assertEqual(k.get_contents_as_string(), s2)
